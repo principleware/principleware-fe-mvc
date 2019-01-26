f5026fb3b726bbde1375d5ccd0986db7c1247e20
Tom <xiaolong@cse.tamu.edu>
2019-01-26 10:49:36 -0500

    Fix scrpts for doc

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,15 @@ const { src, dest, parallel } = require('gulp');
 const typedoc = require("gulp-typedoc");
 
 function doc() {
-    return src(["src/**/*.ts"])
+    return src(["projects/polpware/fe-mvc/src/lib/**/*.ts"])
         .pipe(typedoc({
             name: "Polpware typescript mvc (3.0.0)",            
             out: "docs/",            
             
             module: "commonjs",
             target: "es5",
+
+            exclude: "projects/polpware/fe-mvc/src/lib/**/*.spec.ts",                        
 
             experimentalDecorators: true,
             excludePrivate: true,
