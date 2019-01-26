ecb3baa78c3edd6c7795895b4ada28b0b09940d0
Tom <xiaolong@cse.tamu.edu>
2019-01-26 05:49:58 -0500

    Fix public api for the lib

--- projects/polpware/fe-mvc/src/public_api.ts
+++ projects/polpware/fe-mvc/src/public_api.ts
@@ -1,7 +1,8 @@
-/*
- * Public API Surface of fe-mvc
- */
 
-export * from './lib/fe-mvc.service';
-export * from './lib/fe-mvc.component';
-export * from './lib/fe-mvc.module';
+export * from './lib/mediators/abstract-list';
+export * from './lib/mediators/noop-view-instance';
+export * from './lib/mediators/ngstore-abstract-list';
+export * from './lib/mediators/writable-abstract-list';
+export * from './lib/mediators/interfaces';
+export * from './lib/mediators/rxjs-powered-writable-abstract-list';
+export * from './lib/controllers/list-controller';
