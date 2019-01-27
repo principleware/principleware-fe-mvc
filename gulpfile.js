e195cfd24668417897890d615f98eceac27fe336
Tom <xiaolong@cse.tamu.edu>
2019-01-27 05:55:20 -0500

    Scripts for version

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,22 @@
 const { src, dest, parallel } = require('gulp');
 const typedoc = require("gulp-typedoc");
+const bumpversion = require('gulp-bump');
+
+// `fs` is used instead of require to prevent caching in watch (require caches)
+const fs = require('fs');
+
+function getVersion() {
+    return fs.readFileSync('./VERSION', 'utf8', function(err, data) {
+        return data;
+    });
+};
 
 function doc() {
+    const newVer = getVersion().trim();
+    
     return src(["projects/polpware/fe-mvc/src/lib/**/*.ts"])
         .pipe(typedoc({
-            name: "Polpware typescript mvc (3.0.0)",            
+            name: "Polpware typescript mvc " + newVer,            
             out: "docs/",            
             
             module: "commonjs",
@@ -25,5 +37,20 @@ function doc() {
         }));
 }
 
+function bump() {
+
+    const newVer = getVersion().trim();
+
+// bump versions on package/bower/manifest
+    return src(['./package.json', './projects/polpware/fe-mvc/package.json'])
+        .pipe(bumpversion({
+            version: newVer
+        }))
+        .pipe(dest(function(x) {
+            return x.base;
+        }));
+}
+
+exports.bump = bump;
 exports.doc = doc;
 exports.default = doc;
